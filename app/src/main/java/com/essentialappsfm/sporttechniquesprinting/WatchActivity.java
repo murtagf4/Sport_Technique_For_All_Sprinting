21dd73b134ee4f68f39657582460dbbb4caacf77
Fergus Murtagh <fergus.murtagh4@mail.dcu.ie>
2015-05-28 13:07:03 +0100

    Ready For Demo

--- app/src/main/java/com/essentialappsfm/sporttechniquesprinting/WatchActivity.java
+++ app/src/main/java/com/essentialappsfm/sporttechniquesprinting/WatchActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.MediaController;
+import android.widget.Toast;
 import android.widget.VideoView;
 
 
